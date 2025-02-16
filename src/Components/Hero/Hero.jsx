44219e397e16cb8719f1647bf018017f16969325
Anaghesh Akula <56759282+akula1123@users.noreply.github.com>
2025-02-16 15:41:19 -0500

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,8 +3,7 @@ import './Hero.css'
 import { useNavigate } from 'react-router-dom'
 import nersc from '../../assets/NERSC_BLACK.svg'
 import nsf from '../../assets/NSF_Small.svg'
-import hero from '../../assets/hero-video.mp4'
-import heroReverse from '../../assets/hero-video-cropped.mp4'
+import heroReverse from '../../assets/hero-video-try.mp4'
 
 const Hero = () => {
   const navigate = useNavigate();
@@ -27,14 +26,13 @@ const Hero = () => {
             </div>
         </div>
         <div className='hero-media'>
-        <div className='mark-cover'></div>
-          <iframe width="1024" height="540" margin-bottom="-20px" className='hero-3d-model' allowfullscreen src="https://v3d.net/16e7"></iframe>
+{/*           <iframe width="1024" height="540" margin-bottom="-20px" className='hero-3d-model' allowfullscreen src="https://v3d.net/16e7"></iframe> */}
             {/* <iframe src="https://my.spline.design/clonercubebinarycopy-d555a954b86abfdaf5cc7e5e9f348c93/" frameborder="0" width="500px" height="550px"></iframe> */}
             {/* <img src="src\assets\CriticalNucleus_dimer-trimer-new1-ezgif.com-censor.gif" alt="" /> */}
-            {/* <video src={heroReverse}  autoPlay
+            <video src={heroReverse}  autoPlay
         loop
         muted
-        playsInline></video> */}
+        playsInline></video>
         </div>
       </div>      
     </div>
